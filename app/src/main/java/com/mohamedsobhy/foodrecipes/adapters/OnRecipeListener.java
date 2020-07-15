9b6b6ec76e158ef580289edcf27fbee4a61f774d
m7md.sob7y <mohamedsobhy@paymobsolutions.com>
2020-07-15 13:52:29 +0200

    Initial commit

---
+++ app/src/main/java/com/mohamedsobhy/foodrecipes/adapters/OnRecipeListener.java
@@ -0,0 +1,8 @@
+package com.mohamedsobhy.foodrecipes.adapters;
+
+public interface OnRecipeListener {
+
+    void onRecipeClick(int position);
+
+    void onCategoryClick(String category);
+}
---
+++ app/src/main/java/com/mohamedsobhy/foodrecipes/viewmodels/RecipeViewModel.java
@@ -0,0 +1,44 @@
+package com.mohamedsobhy.foodrecipes.viewmodels;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.mohamedsobhy.foodrecipes.models.Recipe;
+import com.mohamedsobhy.foodrecipes.repositories.RecipeRepository;
+
+public class RecipeViewModel extends ViewModel {
+
+    private RecipeRepository mRecipeRepository;
+    private String mRecipeId;
+    private boolean mDidRetrieveRecipe;
+
+    public RecipeViewModel() {
+        mRecipeRepository = RecipeRepository.getInstance();
+        mDidRetrieveRecipe = false;
+    }
+
+    public LiveData<Recipe> getRecipe(){
+        return mRecipeRepository.getRecipe();
+    }
+
+    public LiveData<Boolean> isRecipeRequestTimedOut(){
+        return mRecipeRepository.isRecipeRequestTimedOut();
+    }
+
+    public void searchRecipeById(String recipeId){
+        mRecipeId = recipeId;
+        mRecipeRepository.searchRecipeById(recipeId);
+    }
+
+    public String getRecipeId() {
+        return mRecipeId;
+    }
+
+    public void setRetrievedRecipe(boolean retrievedRecipe){
+        mDidRetrieveRecipe = retrievedRecipe;
+    }
+
+    public boolean didRetrieveRecipe(){
+        return mDidRetrieveRecipe;
+    }
+}
